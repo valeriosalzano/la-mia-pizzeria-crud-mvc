d2b4f52dafbcbbde94abaaa69cf9414b1b9075a3
Valerio Salzano <vlr.slzn@gmail.com>
2023-09-29 12:16:12 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Database/PizzeriaContext.cs
@@ -0,0 +1,15 @@
+﻿using la_mia_pizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Database
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFVideogameContext;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace la_mia_pizzeria.Models
+{
+    public class Pizza
+    {
+        public string PizzaId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
