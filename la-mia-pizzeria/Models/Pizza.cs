9f8ad1299daebdd9740a6e7130005242b2a11c17
Valerio Salzano <vlr.slzn@gmail.com>
2023-10-02 17:14:08 +0200

    BONUS "wordscount" validationattribute

--- la-mia-pizzeria/Models/Pizza.cs
+++ la-mia-pizzeria/Models/Pizza.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using la_mia_pizzeria.ValidationAttributes;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -12,15 +13,20 @@ namespace la_mia_pizzeria.Models
     {
         [Column(name:"id"), Key]
         public int PizzaId { get; set; }
+
         [Column(name:"name", TypeName = "VARCHAR(100)"), Required]
         public string? Name { get; set; }
+
         [Column(name: "slug", TypeName = "VARCHAR(100)"), Required]
         public string? Slug { get; set; }
-        [Column(name:"price", TypeName = "DECIMAL(5, 2)"), Range(0.00,999.99),Required]
+
+        [Column(name:"price", TypeName = "DECIMAL(5, 2)"), Range(0.01,999.99),Required]
         public decimal Price { get; set; }
-        [Column(name: "description", TypeName = "VARCHAR(1000)")]
-        public string? Description { get; set; } = "";
+
+        [Column(name: "description", TypeName = "VARCHAR(1000)"), WordsCount(Min = 5)]
+        public string? Description { get; set; }
+
         [Column(name: "img_path", TypeName = "VARCHAR(1000)")]
-        public string? ImgPath { get; set; } = "";
+        public string? ImgPath { get; set; }
     }
 }
---
+++ la-mia-pizzeria/ValidationAttributes/WordsCount.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria.ValidationAttributes
+{
+    public class WordsCount : ValidationAttribute
+    {
+        public int Min { get; set; } = 1;
+        public int Max { get; set; } = int.MaxValue;
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            
+
+            if (value is string)
+            {
+                string input = (string)value ?? "";
+                int wordsCount = input.Trim().Split(" ").Length;
+
+                if (wordsCount < Min)
+                {
+                    return new ValidationResult($"Field must contain {Min} words at least");
+                }
+                if (wordsCount > Max) 
+                {
+                    return new ValidationResult($"Field must contain no more than {Max} words");
+                }
+
+                return ValidationResult.Success;
+
+            }
+
+            return new ValidationResult("Invalid field value");
+
+
+        }
+    }
+}
